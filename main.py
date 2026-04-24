f847950ebc43eb3c5c28dbc8bbe8ca6a3e6a1b92
juanara <42655698+juanara@users.noreply.github.com>
2026-04-24 16:39:41 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,12 +6,13 @@ TOKEN = os.getenv("TOKEN")
 
 async def event(update: Update, context: ContextTypes.DEFAULT_TYPE):
     media = [
-        InputMediaPhoto("AAMCBQADGQEDCmM_aesGnHT0ask7yVmTxXnB6Xn1y_kAApobAAI2XFhXpj2CcTONJP4BAAdtAAM7BA",
-                        caption="이벤트 문의는 아래 양식으로만 접수합니다.
+        InputMediaPhoto(
+            "AAMCBQADGQEDCmM_aesGnHT0ask7yVmTxXnB6Xn1y_kAApobAAI2XFhXpj2CcTONJP4BAAdtAAM7BA",
+            caption="""이벤트 문의는 아래 양식으로만 접수합니다.
 
 양식에 맞추지 않고 작성한 문의건은 처리 불가하며, 앞으로 양식 미준수 건은 자동 참여불가 처리합니다.
 
- 번거로우시더라도 원활한 처리를 위해 반드시 지켜주시기 바랍니다.
+번거로우시더라도 원활한 처리를 위해 반드시 지켜주시기 바랍니다.
 
 사이트 :
 닉네임 :
@@ -33,13 +34,13 @@ async def event(update: Update, context: ContextTypes.DEFAULT_TYPE):
 참여금액 :
 이벤트내용 :
 
- 양식 미준수(예: “이벤트주세요”, “참여요” 등 간단 문의)는 접수 불가합니다. 
+양식 미준수(예: “이벤트주세요”, “참여요” 등 간단 문의)는 접수 불가합니다.
 
- 반복 안내 후에도 양식 미준수 시 향후 이벤트 참여 제한이 있을 수 있으니 협조 부탁드립니다. ☺️
-
+반복 안내 후에도 양식 미준수 시 향후 이벤트 참여 제한이 있을 수 있으니 협조 부탁드립니다. ☺️
 
 💕사이트 이벤트 = 사이트 고객센터
-💕가족방 이벤트 = 연합총장.SITE 💕"),
+💕가족방 이벤트 = 연합총장.SITE 💕"""
+        ),
         InputMediaPhoto("AAMCBQADGQEDCmNAaesGnIAW3-pmeSScoCJxL432ZMEAApsbAAI2XFhXIGKgBn8d8IgBAAdtAAM7BA"),
         InputMediaPhoto("AAMCBQADGQEDCmNBaesGnKx5XT8a8E8Ga2RUJa7F6qUAApwbAAI2XFhXtyweiMmGAzMBAAdtAAM7BA"),
         InputMediaPhoto("AAMCBQADGQEDCmNEaesGnA2IEzOqZHOW0Rq_gLDDEEQAAp4bAAI2XFhXarAfDgUPRYABAAdtAAM7BA"),
@@ -49,6 +50,7 @@ async def event(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ]
 
     await update.message.reply_media_group(media)
+
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(CommandHandler("이벤트", event))
